d8b00a98c65e4bbf201275f81d8fc9768dc15c61
Tim Griffith <grifmang@gmail.com>
2019-12-08 20:53:43 -0500

    Done finished half stretch. Need to figure out LS.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
+import { ProductContext } from "./contexts/ProductContext";
+import { CartContext } from "./contexts/CartContext";
 
 // Components
 import Navigation from './components/Navigation';
@@ -13,28 +15,63 @@ function App() {
 
 	const addItem = item => {
 		// add the given item to the cart
+		if (cart) {
+			let count = 0;
+			cart.map(element => {
+				if (element.id === item.id) {
+					count += 1
+				}
+			})
+			if (count === 0) {
+				setCart([...cart, item]);
+			}
+		} else {
+			setCart([item]);
+		}
 	};
+
+	const removeItem = id => {
+		let newArray = [];
+		cart.map(element => {
+			if (element.id !== id) {
+				newArray.push(element);
+			}
+		})
+		setCart(newArray);
+	}
+
+	// useEffect(() => {
+	// 	if (window.localStorage.getItem('cart')) {
+	// 		const cartJSON = window.localStorage.getItem(('cart'));
+	// 		console.log(cartJSON);
+	// 		// setCart(JSON.parse(window.localStorage.getItem('cart')));
+	// 	}
+	// },[cart])
+
+	// useEffect(() => {
+	// 	if (cart) {
+	// 		window.localStorage.setItem('cart', JSON.stringify(cart));
+	// 	}
+	// }, [cart])
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
-
-			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
+			<ProductContext.Provider value={{ products, addItem }}>
+				<CartContext.Provider value={{ cart, removeItem }}>
+					<Navigation />
+				
+				{/* Routes */}
+					<Route
+						exact
+						path="/"
+						component={Products}
 					/>
-				)}
-			/>
-
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
+					<Route
+						path="/cart"
+						component={ShoppingCart}
+					/>
+				</CartContext.Provider>
+			</ProductContext.Provider>
 		</div>
 	);
 }
